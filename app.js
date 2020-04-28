23f81998a439b8cbf1472d2d957334290212658e
Samuel Guevara <samsjamin@outlook.com>
2020-04-28 00:22:22 -0400

    added all questions for app.js

--- app.js
+++ app.js
@@ -39,4 +39,122 @@ function createProfile() {
                 break
         }
     })
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "engineerName"
+            },
+
+            {
+                type: "input",
+                message: "What is your employee ID?",
+                name: "engineerId"
+            },
+
+            {
+                type: "input",
+                message: "What is your email?",
+                name: "engineerEmail"
+            },
+
+            {
+                type: "input",
+                message: "What is your GitHub username?",
+                name: "engineerUsername"
+            },
+
+        ]).then(userChoice => {
+        
+            console.log(userChoice);
+            
+            const engineer = new engineer(userChoice.engineerName, userChoice.engineerId, userChoice.engineerEmail, userChoice.engineerUsername)
+
+            employees.push(engineer)
+
+            createProfile();
+        })
+    }
+
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "internName"
+            },
+
+            {
+                type: "input",
+                message: "What is your employee ID?",
+                name: "internId"
+            },
+
+            {
+                type: "input",
+                message: "What is your email?",
+                name: "internEmail"
+            },
+
+            {
+                type: "input",
+                message: "Which school are you from?",
+                name: "internSchool"
+            },
+
+        ]).then(userChoice => {
+        
+            console.log(userChoice);
+            
+            const intern = new intern(userChoice.internName, userChoice.internId, userChoice.internEmail, userChoice.internSchool)
+
+            employees.push(intern)
+
+            createProfile();
+        })
+    }
+
+    function addManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "managerName"
+            },
+
+            {
+                type: "input",
+                message: "What is your employee ID?",
+                name: "managerId"
+            },
+
+            {
+                type: "input",
+                message: "What is your email?",
+                name: "managerEmail"
+            },
+
+            {
+                type: "input",
+                message: "What is your GitHub username?",
+                name: "managerOfficeNum"
+            },
+
+        ]).then(userChoice => {
+        
+            console.log(userChoice);
+            
+            const manager = new manager(userChoice.managerName, userChoice.managerId, userChoice.managerEmail, userChoice.managerOfficeNum)
+
+            employees.push(manager)
+
+            createProfile();
+        })
+    }
 }
+
+module.exports = employees
+
+createProfile();
--- display.js
+++ display.js
@@ -7,4 +7,22 @@ const dirTemp = path.resolve(__dirname, "../Employee-Summary/templates");
 
 const display = (employees) => {
     const html = [];
+    
+    html.push(employees.filter(employees => employees.getRole() === 'Engineer')
+    .map(engineer => displayEngineer(engineer))
+    );
+
+    html.push(employees.filter(employees => employees.getRole() === 'Intern')
+    .map(intern => displayIntern(intern))
+    );
+    html.push(employees.filter(employees => employees.getRole() === 'Manager')
+    .map(manager => displayManager(manager))
+    );
+
+
+    return displayMain(html.join(''));
+};
+
+const displayManager = manager => {
+    let temp = fs.readFileSync(path.resolve(dirTemp))
 }

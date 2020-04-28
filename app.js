0cf2527d9db3af060c6bec2e9810c32a1947ea12
Samuel Guevara <samsjamin@outlook.com>
2020-04-27 21:46:46 -0400

    updated variables

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
+var employee = require('./lib/employee');
+var engineer = require('./lib/engineer');
+var intern = require('./lib/intern');
+var manager = require('./lib/manager');
+

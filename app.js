8b2e2a2f808158468a756f6326130602af0feaa8
Samuel Guevara <samsjamin@outlook.com>
2020-04-27 22:01:00 -0400

    added inquirer prompt

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ var engineer = require('./lib/engineer');
 var intern = require('./lib/intern');
 var manager = require('./lib/manager');
 
+const employees = []
+
+function createProfile() {
+    inquirer.prompt([
+        { type: "list",
+        name: "employee",
+        message: "What is your position?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+            
+        ]
+
+    }
+    ])
+}
